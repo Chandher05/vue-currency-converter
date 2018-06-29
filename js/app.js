5c30b0addf5eac8aa370ceb8435387b560379681
Kati Frantz <frantzenterprises777@gmail.com>
2018-06-29 07:37:30 +0100

    convert currency and display result

--- js/app.js
+++ js/app.js
@@ -7,11 +7,15 @@ new Vue({
     currencies: {},
     to: 'EUR',
     from: 'USD',
-    amount: 0 
+    amount: 0,
+    result: 0
   }, 
   computed: {
     formattedCurrencies() {
       return Object.values(this.currencies)
+    },
+    calculateResult() {
+      return (Number(this.amount) * this.result).toFixed(3)
     }
   },
   methods: {
@@ -28,6 +32,13 @@ new Vue({
           const { results } = response.data
           localStorage.setItem('currencies', JSON.stringify(results))
           this.currencies = results
+        })
+    },
+    convertCurrency() {
+      axios.get(`https://free.currencyconverterapi.com/api/v5/convert?q=${this.from}_${this.to}`)
+        .then(response => {
+          console.log(response)
+          this.result = response.data.results[`${this.from}_${this.to}`].val
         })
     }
   }
